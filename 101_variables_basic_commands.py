f226e449743a7ce7509348bd80940160a3e9aabc
Charles Poullet <CPoullet@spartaglobal.com>
2020-02-04 14:56:44 +0000

    Started basic variables

---
+++ 101_variables_basic_commands.py
@@ -0,0 +1,36 @@
+# Variables in Python
+    # A variable is like a box
+
+# Assign a variable
+box_pillows = 'Pillows and stuff'
+
+# Look inside a box
+print(box_pillows)
+
+# Re assignment of a variable (changing the stuff inside a box)
+box_pillows = 'Books'
+print(box_pillows)
+
+# Re assignment to different data type
+box_pillows = 14
+print(box_pillows)
+
+# Important functions in Python
+# print()
+
+print('hello hello I am a string')
+print(box_pillows)
+
+# type()
+# returns the data type of the variable
+
+print(type(box_pillows))
+
+
+# input()
+ # input prompts user for input
+
+print('Hey there old sport, what is your name?    ')
+user_response = input()
+
+print(user_response)
