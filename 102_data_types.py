f78dcb9d732f0824aace33296dcbb3963355422c
Charles Poullet <CPoullet@spartaglobal.com>
2020-02-04 15:30:51 +0000

    Expanded on data types

--- 102_data_types.py
+++ 102_data_types.py
@@ -22,3 +22,34 @@ name = 'Lana'
 welcome_message = "Welcome to the DANGEERRZOOOOOOOONE {}".format(name)
 welcome_message2 = f"{name}, welcome to the DANGEERRZOOOOOOOONE"
 print(welcome_message2)
+
+# place an f behind the string and use {} inside the string to
+# interpolate variable/python
+
+# Special methods for strings
+
+my_stringy = "      Hello this is an amazing string"
+
+# .count() method
+print(my_stringy.count('i'))
+
+# .strip() removes trailing white spaces
+print(my_stringy.strip())
+
+# len() - function not a method!
+print(len(my_stringy))
+print(len(my_stringy.strip()))
+
+# .capitalize() - capitalises the first letter and everything else underscore
+print(my_stringy.strip().capitalize())
+
+# .upper() - capitalises every character
+print(my_stringy.upper())
+
+# .lower() - lowercases every character
+print(my_stringy.lower())
+
+# .split() - splits the characters
+
+print(my_stringy.split())
+
