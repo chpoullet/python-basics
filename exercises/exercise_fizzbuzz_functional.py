936f3cfba750deea692882864b7c1b65b561997f
Charles Poullet <CPoullet@spartaglobal.com>
2020-02-06 22:22:50 +0000

    Added comments to fizzbuzz

--- exercises/exercise_fizzbuzz_functional.py
+++ exercises/exercise_fizzbuzz_functional.py
@@ -1,35 +1,37 @@
 from time import sleep
 
 def check(checknum):
+    return(checknum % 15 == 0)
+
+def check2(checknum):
     return(checknum % 3 == 0)
 
-def check2(checknum2):
-    return(checknum2 % 15 == 0)
-
-def check3(checknum3):
-    return(checknum3 % 5 == 0)
+def check3(checknum):
+    return(checknum % 5 == 0)
 
 while True:
     num = int(input('Please input your number (type 0000 to end the game):    '))
-    count_num = 1
+    count_num = 1       # counter starts from 1
 
-    if num == 0000:
+    if num == 0000:    # if the user types 0000, process stops
         break
 
-    while num >= count_num:
-            if check2(count_num):
+    while num >= count_num:   # while the user input number is greater than or equal to count_num,
+                              # the while loop will run
+
+        if check(count_num):
                 print('Fizzbuzz')
                 sleep(0.1)
 
-            elif check(count_num):
+        elif check2(count_num):
                 print('Fizz')
                 sleep(0.1)
 
-            elif check3(count_num):
+        elif check3(count_num):
                 print('Buzz')
                 sleep(0.1)
 
-            else:
+        else:
                 print(count_num)
                 sleep(0.1)
-            count_num += 1
+        count_num += 1
