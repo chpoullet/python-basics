d7c86dfe18c2e3f3532387da5b9b892b1c9a7d6e
Charles Poullet <CPoullet@spartaglobal.com>
2020-02-06 22:02:54 +0000

    Finished miyagi

--- exercises/exercise_miyagi.py
+++ exercises/exercise_miyagi.py
@@ -6,14 +6,32 @@
 # Follow these rules:
 #
 # every time you ask a question --> Mr. Miyagi responde with
-    # --> 'questions are wise, but for now. Wax on, and Wax off!'
+    # --> 'questions are wise, but for now. Wax on, and Wax off!'  (DONE)
 # every statement/question must start with Sensei, otherwise:
-    # --> 'You are smart, but not wise - address me as Sensei please'
+    # --> 'You are smart, but not wise - address me as Sensei please' (DONE)
 # every time you mention 'block' or 'blocking' --> Mr. Miyagi responde with
-    # --> 'Remeber, best block, not to be there..'
+    # --> 'Remeber, best block, not to be there..' (DONE)
 # anything else you say:
-    # --> 'do not lose focus. Wax on. Wax off.'
+    # --> 'do not lose focus. Wax on. Wax off.' (DONE)
 
 # Make it so you keep playing until we say: 'Sensei, I am at peace'
-    # --> 'Sometimes, what heart know, head forget'
+    # --> 'Sometimes, what heart know, head forget' (DONE)
 
+while True:
+    question = input("Ask Mr. Miyagi a question:    ").lower().strip()
+
+    if question.find('sensei') == -1:
+        print('You are smart, but not wise - address me as Sensei please')
+
+    elif 'block' in question or 'blocking' in question:
+        print('Remember, best block, not to be there.')
+
+    elif question == 'sensei, i am at peace':
+        print('Sometimes, what heart know, head forget')
+        break
+
+    elif question[-1] == '?':
+        print('Questions are wise, but for now. Wax on, and Wax off!')
+
+    else:
+        print("Do not lose focus. Wax on. Wax off.")
